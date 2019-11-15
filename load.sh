c0fc0adced164ec32129642d65376641cc48e2d0
lxohi <16010388+lxohi@users.noreply.github.com>
2019-11-15 12:26:31 +0800

    load.sh: limit memory usage of sbt launcher to 256MB

--- load.sh
+++ load.sh
@@ -6,7 +6,7 @@ cd dummyProject
 for version in $(cat ../SBT_VERSIONS); do
     echo "==================== Loading $verion... ===================="
     echo $version >project/build.properties
-    ../sbt/bin/sbt sbtVersion
+    ../sbt/bin/sbt -mem 256 sbtVersion
 done 
 
 echo "Finished!"
